8c2c9a4f00f64fcf320cb39c873fa1dbb5f22c8c
FilipZajdel <filip97.zajdel@gmail.com>
2020-02-03 19:05:19 +0100

    Add timing measurement

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,24 @@
 import _test_00_dll as dll_tests
+from datetime import datetime
+import sys
+import json
 
-dll_tests.Test_TransposeWords16x16()
-# dll_tests.Test_Deinterleve_16Bytes_to_2x8Bytes()
-#dll_tests.Test_TransposeByte8x8()
-#dll_tests.Test_TransposeBits_16xI8_to_8xI16()
-#dll_tests.Test_TransposeBits_16xI8_to_8xI16_perform()
-#dll_tests.Test_Deinterleve_14x8Words_to_8x14Words()
-#dll_tests.Test_TransposeBits_14xI16_to_16xI16()
+def save_report_as_json(filename, report):
+    with open(f"{filename}.json", "w") as f_report:
+        json.dump(report, f_report)
+
+def current_time_as_str():
+    return datetime.now().strftime("%m-%d-%Y-%H:%M:%S")
+
+
+if __name__ == "__main__":
+    report = {"datetime":current_time_as_str(), "platform": f"{sys.platform}",
+    "functions":{}}
+    now = current_time_as_str()
+
+    report["functions"]["TransposeWords16x16"] = dll_tests.Test_TransposeWords16x16()
+    report["functions"]["TransposeWords8x8"] = dll_tests.Test_TransposeByte8x8()
+
+    save_report_as_json(f"test_run_{now}", report)
+    print(f"Results of tests on {sys.platform} run at {now}: \n{report}")
+    
