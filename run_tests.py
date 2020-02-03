4c52529999919c9d01b87ae7007f55b10fb70c3d
FilipZajdel <filip97.zajdel@gmail.com>
2020-02-03 18:41:25 +0100

    Make directory for results and fills it with one file

--- run_tests.py
+++ run_tests.py
@@ -15,10 +15,11 @@ if __name__ == "__main__":
     report = {"datetime":current_time_as_str(), "platform": f"{sys.platform}",
     "functions":{}}
     now = current_time_as_str()
+    report_dir = "Results"
 
     report["functions"]["TransposeWords16x16"] = dll_tests.Test_TransposeWords16x16()
     report["functions"]["TransposeWords8x8"] = dll_tests.Test_TransposeByte8x8()
 
-    save_report_as_json(f"test_run_{now}", report)
+    save_report_as_json(f"Results/test_run_{now}", report)
     print(f"Results of tests on {sys.platform} run at {now}: \n{report}")
     
